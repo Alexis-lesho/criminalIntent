80d566b37930a50df6961725d74783e65b9777f1
Alexis-lesho <14470226@itcampeche.edu.mx>
2017-12-18 23:33:37 -0600

    criminalIntent:

---
+++ app/src/main/java/com/alexistrejo/android/contadorfragmentos/MainActivity.java
@@ -0,0 +1,24 @@
+package com.alexistrejo.android.contadorfragmentos;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import static com.alexistrejo.android.contadorfragmentos.R.layout.fragment_contador;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null){
+            fragment = new ContadorFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
+    }
+}
